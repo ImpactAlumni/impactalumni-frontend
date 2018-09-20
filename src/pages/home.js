8bebcb51023bc8c12347f0b08e3864132307e10c
Michael Yosavani Putra <surbaktievan@ymail.com>
2018-09-21 00:12:01 +0700

    fixing grammer

--- src/pages/home.js
+++ src/pages/home.js
@@ -79,7 +79,7 @@ class Home extends Component {
                       fontFamily: "Menlo, Monaco, 'Courier New', monospace"
                     }}
                   >
-                    We Help Advance Indonesian Technology
+                    Get advice from experts.
                   </Header>
                   <p
                     style={{
@@ -87,27 +87,11 @@ class Home extends Component {
                       fontFamily: "Menlo, Monaco, 'Courier New', monospace"
                     }}
                   >
-                    We are aware that technological advances are now growing
-                    rapidly, and every country must be able to follow it. that's
-                    one reason why we want to be a developer.
-                  </p>
-                  <Header
-                    as="h3"
-                    style={{
-                      fontSize: "2em",
-                      fontFamily: "Menlo, Monaco, 'Courier New', monospace"
-                    }}
-                  >
-                    We create an alumni community
-                  </Header>
-                  <p
-                    style={{
-                      fontSize: "1.33em",
-                      fontFamily: "Menlo, Monaco, 'Courier New', monospace"
-                    }}
-                  >
-                    yes, by making our alumni community easier to exchange
-                    information.
+                    Being part of a large, diverse group gives you many
+                    opportunities to get advice from people who’ve gone through
+                    it all. What to do. What not to do. Their experiences can
+                    point you in the right direction and help you avoid many
+                    costly pitfalls.
                   </p>
                 </Grid.Column>
                 <Grid.Column floated="right" width={6}>
@@ -145,17 +129,20 @@ class Home extends Component {
                       fontFamily: "Menlo, Monaco, 'Courier New', monospace"
                     }}
                   >
-                    "Why the alumni community?"
+                    What is our purpose?
                   </Header>
                   <p
                     style={{
                       fontSize: "1.33em",
-                      fontFamily: "Menlo, Monaco, 'Courier New', monospace"
+                      fontFamily: "Menlo, Monaco, 'Courier New', monospace",
+                      textAlign: "left"
                     }}
                   >
-                    Alumni can be a source of information on the world of work &
-                    business for new graduates of a boot camp, in addition to
-                    being an inspiration for students in today's Bootcamp.
+                    One of our main purposes is to support a network of former
+                    graduates who will, in turn, help to raise the profile of
+                    the boot camp. Alumni associations aim to bring together
+                    like-minded individuals and offer a broader networking
+                    scope.
                   </p>
                 </Grid.Column>
                 <Grid.Column
@@ -200,9 +187,11 @@ class Home extends Component {
           vertical="vertical"
         >
           {" "}
-          <div className="middle3-kata">"First, Solve The Problem.</div>
-          <div className="middle3-kata1">Then, Write The Code"</div>
-          <div className="middle3-kata2">- John Jhonson</div>
+          <Container>
+            <div className="middle3-kata">"First, Solve The Problem.</div>
+            <div className="middle3-kata1">Then, Write The Code"</div>
+            <div className="middle3-kata2">- John Jhonson</div>
+          </Container>
         </Segment>
         {/* middle4 */}
         <Container>
@@ -234,21 +223,21 @@ class Home extends Component {
                     as="h3"
                     style={{
                       fontSize: "2em",
-                      fontFamily: "Menlo, Monaco, 'Courier New', monospace"
+                      fontFamily: "Menlo, Monaco, 'Courier New', monospace",
+                      textAlign: "left"
                     }}
                   >
-                    Programmer never be able to code alone
+                    Happiness is not something ready made. It comes from your
+                    own actions.
                   </Header>
                   <p
                     style={{
                       fontSize: "1.33em",
-                      fontFamily: "Menlo, Monaco, 'Courier New', monospace"
+                      fontFamily: "Menlo, Monaco, 'Courier New', monospace",
+                      textAlign: "right"
                     }}
                   >
-                    Programmers have communities like GitHub, GitLab etc. from
-                    that community the development in the world of coding is
-                    very fast. Our fellow programmers help each other solve
-                    problems
+                    - 14th Dalai Lama
                   </p>
                 </Grid.Column>
               </Grid.Row>
